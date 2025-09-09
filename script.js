6c166f6cc9af5cd3b43ea44bd30c7081046282c0
Victor <vicmoral370@gmail.com>
2025-09-09 15:00:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,109 @@
+const screen = document.getElementById("display-text");
+const numberedButtons = Array.from(document.getElementsByClassName("number"));
+const operatorButtons = Array.from(document.getElementsByClassName("operator"));
+
+let memory = 0;
+let a = 0;
+let b = 0;
+
+let addition = false;
+let subtraction = false;
+let multiplication = false;
+let division = false;
+
+function operation(x, y, operator) {
+    if (operator === "+") {
+        return x + y;
+    }
+
+    if (operator === "-") {
+        return x - y;
+    }
+
+    if (operator === "x") {
+        return x * y;
+    }
+
+    if (operator === "÷") {
+        return x / y;
+    }
+}
+
+function resetOperations() {
+    addition = false;
+    subtraction = false;
+    multiplication = false;
+    division = false;
+}
+
+
+numberedButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (screen.textContent.length < 8) {
+            if (addition || subtraction || multiplication || division) {
+                screen.textContent += button.textContent.trim();
+                b = Number(screen.textContent);
+            }
+            else {
+                screen.textContent += button.textContent.trim();
+            }
+        }
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.querySelector("span").textContent === "C") {
+            screen.textContent = "";
+            resetOperations();
+        }
+
+        if (button.querySelector("span").textContent === "+") {
+            a = Number(screen.textContent);
+            screen.textContent = ""
+            resetOperations();
+            addition = true;
+        }
+
+        if (button.querySelector("span").textContent === "-") {
+            a = Number(screen.textContent);
+            screen.textContent = ""
+            resetOperations();
+            subtraction = true;
+        }
+
+        if (button.querySelector("span").textContent === "x") {
+            a = Number(screen.textContent);
+            screen.textContent = ""
+            resetOperations();
+            multiplication = true;
+        }
+
+        if (button.querySelector("span").textContent === "÷") {
+            a = Number(screen.textContent);
+            screen.textContent = ""
+            resetOperations();
+            division = true;
+        }
+
+        if (button.querySelector("span").textContent === "=") {
+            if (addition) {
+                memory = operation(a, b, "+")
+            }
+            if (subtraction) {
+                memory = operation(a, b, "-")
+            }
+            if (multiplication) {
+                memory = operation(a, b, "x")
+            }
+            if (division) {
+                memory = operation(a, b, "÷")
+            }
+            screen.textContent = memory;
+        }
+
+        if (button.querySelector("span").textContent === "+/-") {
+            screen.textContent = String((screen.textContent) * -1);
+        }
+    })
+})
